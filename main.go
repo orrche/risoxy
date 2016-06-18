6c640add34915d3247f697ebc3dc110bcaf2ac3b
Kent Gustavsson <kent@minoris.se>
2016-06-18 13:33:14 +0200

    Lots of changes  * Changed listening port from localhost:8080 to :8080, still to much debugging, this is more inconvinient then good  * Use toml instead of json  * Saves state  * Add EntryPoint
    Signed-off-by: Kent Gustavsson <kent@minoris.se>

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"bufio"
 	"encoding/base64"
-	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"log"
@@ -11,22 +10,30 @@ import (
 	"os"
 	"os/exec"
 	"strings"
+
+	"github.com/BurntSushi/toml"
 )
 
 type EndPoint struct {
 	Destination string
-	Start       *Node
 }
 
 type Node struct {
 	Key    string
 	NodeId string
 
-	EndPoint []EndPoint
+	EndPoints []EndPoint
+}
+
+type EntryPoint struct {
+	Domain string
+	Path   string
+	Node   *Node
 }
 
 type Manager struct {
-	Nodes []Node
+	Nodes       []Node
+	EntryPoints []EntryPoint
 }
 
 func GetStartPointForm(w http.ResponseWriter, r *http.Request) {
@@ -36,14 +43,40 @@ func GetStartPointForm(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "EndPoint: <input name='endpoint'/><br/>")
 	fmt.Fprintf(w, "<input type='submit'/>")
 	fmt.Fprintf(w, "</form>")
-
 }
 
 func StartPointFData(w http.ResponseWriter, r *http.Request) *Node {
 	mgmtPoint := Node{r.FormValue("key"), r.FormValue("domain"), make([]EndPoint, 0, 10)}
-	mgmtPoint.EndPoint = append(mgmtPoint.EndPoint, EndPoint{r.FormValue("endpoint"), &mgmtPoint})
+	mgmtPoint.EndPoints = append(mgmtPoint.EndPoints, EndPoint{r.FormValue("endpoint")})
 
 	return &mgmtPoint
+}
+
+func GetEntryPointForm(w http.ResponseWriter, r *http.Request, manager *Manager) {
+	fmt.Fprintf(w, "<form method='post'/>")
+	fmt.Fprintf(w, "Domain: <input name='domain'/><br/>")
+	fmt.Fprintf(w, "Path: <input name='path' value='/'><br/>")
+	fmt.Fprintf(w, "Node: <select name='node'>")
+
+	for _, node := range manager.Nodes {
+		fmt.Fprintf(w, "<option value='%s'>%s</option>", node.NodeId, node.NodeId)
+	}
+
+	fmt.Fprintf(w, "</select>")
+	fmt.Fprintf(w, "<input type='submit'/>")
+	fmt.Fprintf(w, "</form>")
+}
+
+func EntryPointFData(w http.ResponseWriter, r *http.Request, manager *Manager) *EntryPoint {
+	entryPoint := EntryPoint{r.FormValue("domain"), r.FormValue("path"), nil}
+	for _, node := range manager.Nodes {
+		if node.NodeId == r.FormValue("node") {
+			entryPoint.Node = &node
+			break
+		}
+	}
+
+	return &entryPoint
 }
 
 func startNginx() {
@@ -58,7 +91,6 @@ func startNginx() {
 		log.Fatal(err)
 	}
 
-	log.Print("DATA:")
 	for {
 		text, err := reader.ReadString('\n')
 		if err != nil {
@@ -115,8 +147,8 @@ func writeStartPoint(startPoint Node) {
 	fmt.Fprintf(file, "server {\n")
 	fmt.Fprintf(file, "\tlisten 80;\n")
 	fmt.Fprintf(file, "\tserver_name %s;\n", startPoint.NodeId)
-	fmt.Fprintf(file, "\tlocation /{\n")
-	for _, end := range startPoint.EndPoint {
+	fmt.Fprintf(file, "\tlocation / {\n")
+	for _, end := range startPoint.EndPoints {
 		fmt.Fprintf(file, "\t\tproxy_pass %s;\n", end.Destination)
 	}
 	fmt.Fprintf(file, "\t}\n")
@@ -157,6 +189,7 @@ bmY7CgogICAgfQp9Cgo=
 func writeMenu(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
 	fmt.Fprint(w, "<a href='/add'>Add</a>\n")
+	fmt.Fprint(w, "<a href='/entrypoint/add'>Add Entrypoint</a>\n")
 	fmt.Fprint(w, "<a href='/listnodes'>List</a>\n")
 	fmt.Fprint(w, "<a href='/valid'>Validate</a>\n")
 	fmt.Fprint(w, "<a href='/update'>Update</a>\n")
@@ -189,11 +222,34 @@ func (manager *Manager) updateHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func updateConfiguration(manager *Manager) {
+	file, err := os.Create("/etc/nginx/conf.d/risoxy.conf")
+	if err != nil {
+		log.Print(err)
+		return
+	}
+	defer file.Close()
+
+	for _, entryPoint := range manager.EntryPoints {
+
+		fmt.Fprintf(file, "server {\n")
+		fmt.Fprintf(file, "\tlisten 80;\n")
+		fmt.Fprintf(file, "\tserver_name %s;\n", entryPoint.Domain)
+		fmt.Fprintf(file, "\tlocation %s {\n", entryPoint.Path)
+
+		fmt.Fprintf(file, "\t\tproxy_pass %s;\n", entryPoint.Node.EndPoints[0].Destination)
+
+		fmt.Fprintf(file, "\t}\n")
+		fmt.Fprintf(file, "}\n")
+	}
+	saveState(manager)
+}
+
 func (manager *Manager) addNode(node *Node, reload bool) {
 	manager.Nodes = append(manager.Nodes, *node)
 
-	writeStartPoint(*node)
 	if reload {
+		updateConfiguration(manager)
 		reloadNginx()
 	}
 }
@@ -206,7 +262,7 @@ func (manager *Manager) AddNode(node *Node) {
 func (manager *Manager) UpdateNode(newNode *Node) {
 	for _, node := range manager.Nodes {
 		if node.NodeId == newNode.NodeId {
-			node.EndPoint[0] = newNode.EndPoint[0]
+			node.EndPoints[0] = newNode.EndPoints[0]
 			writeStartPoint(node)
 			reloadNginx()
 			return
@@ -220,7 +276,7 @@ func (manager *Manager) listNodeHandler(w http.ResponseWriter, r *http.Request) 
 
 	for _, node := range manager.Nodes {
 		fmt.Fprintf(w, "%s <a href='http://%s'>[w]</a><br/>\n", node.NodeId, node.NodeId)
-		for _, endpoint := range node.EndPoint {
+		for _, endpoint := range node.EndPoints {
 			fmt.Fprintf(w, " %s<br/>\n", endpoint.Destination)
 		}
 	}
@@ -231,12 +287,12 @@ func (manager *Manager) activeNodeHandler(w http.ResponseWriter, r *http.Request
 	if r.Method == "POST" {
 		mgmtPoint := StartPointFData(w, r)
 		for _, node := range manager.Nodes {
-			if node.NodeId == mgmtPoint.NodeId && node.EndPoint[0].Destination == mgmtPoint.EndPoint[0].Destination {
+			if node.NodeId == mgmtPoint.NodeId && node.EndPoints[0].Destination == mgmtPoint.EndPoints[0].Destination {
 				fmt.Fprintf(w, "OK")
 				return
 			} else if node.NodeId == mgmtPoint.NodeId {
 				fmt.Fprintf(w, "Domain ok...")
-				fmt.Fprintf(w, "%s = %s", node.EndPoint[0].Destination, mgmtPoint.EndPoint[0].Destination)
+				fmt.Fprintf(w, "%s = %s", node.EndPoints[0].Destination, mgmtPoint.EndPoints[0].Destination)
 				return
 			}
 		}
@@ -247,34 +303,142 @@ func (manager *Manager) activeNodeHandler(w http.ResponseWriter, r *http.Request
 	}
 }
 
-func LoadManager(filename string) Manager {
-	var manager Manager
-	configFile, err := os.Open(filename)
+func (manager *Manager) addEntryPointHandler(w http.ResponseWriter, r *http.Request) {
+	writeMenu(w, r)
+	if r.Method == "POST" {
+		entryPoint := EntryPointFData(w, r, manager)
+		manager.EntryPoints = append(manager.EntryPoints, *entryPoint)
+		updateConfiguration(manager)
+		reloadNginx()
+	}
+	GetEntryPointForm(w, r, manager)
+}
+
+func failOnError(err error, msg string) {
 	if err != nil {
-		log.Print("Config file read error: ", err)
-		return manager
+		log.Fatalf("%s: %s", msg, err)
+		panic(fmt.Sprintf("%s: %s", msg, err))
 	}
-	jsonParser := json.NewDecoder(configFile)
-	if err = jsonParser.Decode(&manager); err != nil {
-		log.Print("Parse error", err)
+}
+
+type TOML_EndPoint struct {
+	Destination string
+}
+
+type TOML_Node struct {
+	Key      string
+	NodeId   string
+	EndPoint []TOML_EndPoint
+}
+
+type TOML_EntryPoint struct {
+	Domain string
+	Path   string
+	NodeId string
+}
+
+type TOML_Manager struct {
+	Version    string
+	Node       []TOML_Node
+	EntryPoint []TOML_EntryPoint
+}
+
+func saveState(manager *Manager) {
+	var tManager TOML_Manager
+
+	tManager.Version = "0.1"
+	for _, node := range manager.Nodes {
+		var tNode TOML_Node
+		tNode.Key = node.Key
+		tNode.NodeId = node.NodeId
+
+		for _, endpoint := range node.EndPoints {
+			var tEndPoint TOML_EndPoint
+			tEndPoint.Destination = endpoint.Destination
+
+			tNode.EndPoint = append(tNode.EndPoint, tEndPoint)
+
+		}
+		tManager.Node = append(tManager.Node, tNode)
 	}
+
+	for _, entry := range manager.EntryPoints {
+		var tEntry TOML_EntryPoint
+		tEntry.Domain = entry.Domain
+		tEntry.Path = entry.Path
+		tEntry.NodeId = entry.Node.NodeId
+
+		tManager.EntryPoint = append(tManager.EntryPoint, tEntry)
+	}
+
+	file, err := os.Create("/etc/loadmanager/state/risoxy.state")
+	failOnError(err, "Unable to save state file")
+	defer file.Close()
+	enc := toml.NewEncoder(file)
+	enc.Encode(tManager)
+}
+
+func loadState(filename string) Manager {
+	_, err := os.Stat(filename)
+	failOnError(err, "Config file missing")
+
+	var tManager TOML_Manager
+	_, err = toml.DecodeFile(filename, &tManager)
+	failOnError(err, "")
+
+	var manager Manager
+
+	for _, tNode := range tManager.Node {
+		var node Node
+		node.Key = tNode.Key
+		node.NodeId = tNode.NodeId
+
+		for _, tEndPoint := range tNode.EndPoint {
+			var endpoint EndPoint
+			endpoint.Destination = tEndPoint.Destination
+
+			node.EndPoints = append(node.EndPoints, endpoint)
+		}
+
+		manager.Nodes = append(manager.Nodes, node)
+	}
+
+	for _, tEntry := range tManager.EntryPoint {
+		var entry EntryPoint
+		entry.Domain = tEntry.Domain
+		entry.Path = tEntry.Path
+
+		for _, node := range manager.Nodes {
+			if node.NodeId == tEntry.NodeId {
+				entry.Node = &node
+				break
+			}
+		}
+
+		manager.EntryPoints = append(manager.EntryPoints, entry)
+	}
+
+	return manager
+}
+
+func LoadManager(filename string) Manager {
+	manager := loadState(filename)
 	return manager
 }
 
 func main() {
 	writeDefaultConfig()
-	manager := LoadManager("/etc/loadmanager/loadmanager.conf")
+	manager := LoadManager("/etc/loadmanager/state/risoxy.state")
 
-	for _, node := range manager.Nodes {
-		writeStartPoint(node)
-	}
+	updateConfiguration(&manager)
+
 	go startNginx()
 
 	http.HandleFunc("/", handler)
 	http.HandleFunc("/add", manager.addHandler)
 	http.HandleFunc("/update", manager.updateHandler)
+	http.HandleFunc("/entrypoint/add", manager.addEntryPointHandler)
 	http.HandleFunc("/listnodes", manager.listNodeHandler)
 	http.HandleFunc("/valid", manager.activeNodeHandler)
-	http.ListenAndServe("localhost:8080", nil)
-
+	http.ListenAndServe(":8080", nil)
 }
